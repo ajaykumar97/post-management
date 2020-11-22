4f1f9f652144a71d63ee9c70df7d4eef30177d76
ajay <ajaysidhu17@gmail.com>
2020-11-22 13:55:19 +0530

    Updated `App.js` test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders react app', () => {
+  const mockStore = configureStore()
+  const initialState = {post: {posts: []}}
+  let store;
+
+  store = mockStore(initialState)
+
+  render(<Provider store={store}><App /></Provider>);
+  const linkElement = screen.getByText(/Posts Management System/i);
   expect(linkElement).toBeInTheDocument();
 });
